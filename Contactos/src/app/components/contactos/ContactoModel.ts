e5888d65226c1b3931983854084e0fd15a041e13
Javier Rangel <javier.hernandez.rangel@gmail.com>
2018-02-12 11:53:01 -0600

    xcxdd

--- Contactos/src/app/components/contactos/ContactoModel.ts
+++ Contactos/src/app/components/contactos/ContactoModel.ts
@@ -1,8 +1,10 @@
 export class ContactoModel{
+        identifier:string;
         nombre:string;
         telefono:string;
            
         constructor(){
+            this.identifier="";
            this.nombre="";
            this.telefono ="";
         }
--- Contactos/src/app/contactos.service.ts
+++ Contactos/src/app/contactos.service.ts
@@ -1,8 +1,43 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ContactoModel } from './components/contactos/contactoModel';
 
 @Injectable()
 export class ContactosService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getContactos(success: (contactos: ContactoModel[]) => void,
+  error: (message: string) => void
+) {
+
+  let url = 'https://baas.kinvey.com/appdata/kid_ByzNdhm8z/movies';
+  let headers = {
+    'Content-Type': 'application/json',
+    'Authorization': 'Basic a2lkX0J5ek5kaG04ejo4NDg3NTE5MGQwZDE0NWJkOGU4OGZlOTQ0ZDYyNjIxNg==',
+    'X-Kinvey-API-Version': '3'
+  };
+
+  this.http.get(url, { headers: headers }).subscribe(
+    data => {
+      let contactos: ContactoModel[] = [];
+
+      for (let contactoJson of data as Array<any>) {
+        const contacto:ContactoModel = {
+          identifier: contactoJson._id,
+          nombre:contactoJson.titulo,
+          telefono:contactoJson.descripcion
+        };
+        contactos.push(contacto);
+      }
+      success(contactos);
+    },
+    err => {
+      error('Ocurrio un error al consultar la lista de peliculas');
+    });
+
+}
 
 }
